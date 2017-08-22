0938e91869ab66b89e2e9fd7a2cc86ee5f3f8fd4
Lisa Gates <gatesinfo@gmail.com>
2017-08-22 10:56:46 +0200

    master is working now

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 20170822025102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "instruments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
