9f1f889394a19c23a0c33debe0f6379ae790b63d
Marius Fitzworme <Twofirst21@gmail.com>
2017-08-22 17:45:41 +0200

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :instruments, only: [:index, :new, :create, :show]
+  resources :instruments do
+  resources :bookings, only: [:create]
+  end
 end
