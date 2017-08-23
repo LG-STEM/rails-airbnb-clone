7cf14e5d094919e1dbded47638dbd007e3d7d20c
Timur Sarkisov <timur.sarkisov@googlemail.com>
2017-08-23 11:56:58 +0200

    added index page view

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170822132401) do
-=======
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170822121257) do
-=======
-ActiveRecord::Schema.define(version: 20170822130046) do
->>>>>>> master
->>>>>>> 1a616bf1af217e4713992d62a149d6b1603bb751
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
