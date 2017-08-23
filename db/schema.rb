2907f732273414e52307ed4ab684f5420e86ad49
Marius Fitzworme <Twofirst21@gmail.com>
2017-08-23 11:42:48 +0200

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170822130046) do
-=======
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170822132401) do
-=======
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170822121257) do
-=======
-ActiveRecord::Schema.define(version: 20170822130046) do
->>>>>>> master
->>>>>>> 1a616bf1af217e4713992d62a149d6b1603bb751
->>>>>>> 6434c4b63128dd04a545a43e986746ec47bff0a6
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,4 +80,4 @@ ActiveRecord::Schema.define(version: 20170822130046) do
   add_foreign_key "bookings", "instruments"
   add_foreign_key "bookings", "users"
   add_foreign_key "instruments", "users"
-end
+
