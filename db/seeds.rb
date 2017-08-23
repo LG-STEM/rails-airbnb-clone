1b934d82731c4fa17ea5408b8ea407bcb2ea4ede
Alvaro Diaz <albaco81@hotmail.com>
2017-08-23 16:29:49 +0200

    add some index style

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,19 @@ User.delete_all
 puts 'Creating 20 fake instruments...'
 
 INSTRUMENTS = {
-  piano: ['https://usa.yamaha.com/files/DF3BBFBAC0EB418EAA55B39BBDE6691D_12073_2787x2296_b0d03a194b23e7cf85e62e1a36a7c1ab.jpg',
-    'http://bestdigitalpiano.net/wp-content/uploads/2013/09/Roland-RD-300NX-Digital-Piano-1024x515.jpg'],
+  piano: [
+    'https://usa.yamaha.com/files/DF3BBFBAC0EB418EAA55B39BBDE6691D_12073_2787x2296_b0d03a194b23e7cf85e62e1a36a7c1ab.jpg',
+    'http://bestdigitalpiano.net/wp-content/uploads/2013/09/Roland-RD-300NX-Digital-Piano-1024x515.jpg',
+    'https://images-na.ssl-images-amazon.com/images/G/01/img16/musical-instruments/vertical-store/keyboards/mi_vertical_keyboards_short_2._CB278962455_.jpg'
+    ],
   guitar: [
-    'https://static.pexels.com/photos/225230/pexels-photo-225230.jpeg'
-    'https://static.pexels.com/photos/164869/pexels-photo-164869.jpeg'
-    'https://static.pexels.com/photos/15919/pexels-photo.jpg'
-  ]
+    'https://www.taylorguitars.com/sites/default/files/TaylorGuitars-New-for-17-Browse.jpg',
+    'https://cdn.pixabay.com/photo/2015/08/29/14/18/bass-913092_960_720.jpg',
+    'https://www.mopop.org/media/2628/gibson_les_paul_goldtop_guitar_gallery.jpg'
+    ],
+  drums: [
+    'https://s-media-cache-ak0.pinimg.com/736x/97/79/0c/97790cf4d9bac1be384ad4e75e6ca193--gretsch-drums-drum-sets.jpg'
+    ]
 
 }
 
