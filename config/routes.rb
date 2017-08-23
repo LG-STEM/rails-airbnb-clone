e0253bf032296e6dddd3e8e4efcd5e1c1bbfc510
Marius Fitzworme <Twofirst21@gmail.com>
2017-08-23 09:54:21 +0200

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,4 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   resources :instruments, only: [:index, :new, :create, :show]
-  resources :instruments do
-  resources :bookings, only: [:create]
-  end
 end
